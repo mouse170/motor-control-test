18d41c6c8112739533b2faba453acd18aba86ddc
Chu <mouse170@gmail.com>
2015-01-16 15:28:27 +0800

    add smoothly motor control

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var app = require('http').createServer(handler);
 var io = require('socket.io').listen(app);
 var fs = require('fs');
 var piblaster = require('pi-blaster.js');
+var vdegree=0.15;
+var hdegree=0.2;
 app.listen(8124);
 
 function handler(req, res) {
@@ -20,31 +22,39 @@ function puts(error, stdout, stderr) { sys.puts(stdout) }
 
 io.sockets.on('connection', function(socket){
     socket.on('motorControl', function(pos) {
-        
+
         if(pos=='up'){
             //debug
             exec("echo up", puts);
             //control
             exec("node ./assets/scripts/up.js");
             io.sockets.emit('motorLog',pos);
+            hincrease();
+            piblaster.setPwm(23, hdegree);
         }
         else if (pos=='down'){
             //debug
             exec("echo down", puts);
             exec("node ./assets/scripts/down.js");
-           io.sockets.emit('motorLog',pos); 
+           io.sockets.emit('motorLog',pos);
+           hdecrease();
+           piblaster.setPwm(23, hdegree);
         }
         else if (pos=='right'){
             //debug
             exec("echo right", puts);
             exec("node ./assets/scripts/right.js");
             io.sockets.emit('motorLog',pos);
+            vincrease();
+            piblaster.setPwm(24, vdegree);
         }
         else if (pos=='left'){
             //debug
             exec("echo left", puts);
             exec("node ./assets/scripts/left.js");
             io.sockets.emit('motorLog',pos);
+            vdecrease();
+            piblaster.setPwm(24, vdegree);
        }
         else if (pos=='release'){
             exec("sh ./assets/scripts/release.sh", puts);
@@ -67,9 +77,38 @@ io.sockets.on('connection', function(socket){
 });
 
 
-            
-    
-    
+function vincrease(){
+  if (vdegree<0.21){
+    vdegree = parseInt((vdegree* 1e2)+2, 10);
+    console.log(vdegree, vdegree/1e2);
+    vdegree = vdegree/1e2;
+  }
+
+}
+function vdecrease(){
+  if (vdegree>0.08){
+    vdegree = parseInt((vdegree* 1e2)-2, 10);
+    console.log(vdegree, vdegree/1e2);
+    vdegree = vdegree/1e2;
+  }
+}
+function hincrease(){
+  if (hdegree<0.22){
+    hdegree = parseInt((hdegree* 1e2)+2, 10);
+    console.log(hdegree, hdegree/1e2);
+    hdegree = hdegree/1e2;
+  }
+}
+function hdecrease(){
+  if (hdegree>0.16){
+    hdegree = parseInt((hdegree* 1e2)-2, 10);
+    console.log(hdegree, hdegree/1e2);
+    hdegree = hdegree/1e2;
+  }
+}
+
+
+
 
 //function puts(error, stdout, stderr) { sys.puts(stdout) }
 //exec("ls -la", puts);
